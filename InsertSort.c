cdfffd717eaee7622d7b140da1ad19a4fdb29001
Eduarda Soares <edu.soaresferreira@outlook.com.br>
2023-10-09 21:27:53 -0300

    algoritmo insertsort implementado

--- InsertSort.c
+++ InsertSort.c
@@ -1,58 +1,41 @@
-// Código a ser implementado
-
 #include <stdio.h>
 #include <stdlib.h>
- 
-// Define uma constante
-// Define a constant
+
 #define MAX 10
- 
-// Protótipo da função de ordenação
-// Ordination function prototype
+
 void insertion_sort(int *a);
- 
-// Função main
-// Main Function
-int main(int argc, char** argv)
-{
- int i, vet[MAX];
- 
- // Lê MAX ou 10 valores
- // Read MAX or 10 values
- for(i = 0; i < MAX; i++)
- {
-  printf("Digite um valor: ");
-  scanf("%d", &vet[i]);
- }
- 
- // Ordena os valores
- // Order values
- insertion_sort(vet);
- 
- // Imprime os valores ordenados
- // Print values in order ascendant
- printf("nnValores ordenadosn");
- for(i = 0; i < MAX; i++)
- {
-  printf("%dn", vet[i]);
- }
- system("pause");
- return 0;
+
+int main(int argc, char** argv){
+    int i, vet[MAX];
+    for(i = 0; i < MAX; i++)
+    {
+        printf("Digite um valor: ");
+        scanf("%d", &vet[i]);
+    }
+
+    insertion_sort(vet);
+    printf("nnValores ordenadosn");
+    for(i = 0; i < MAX; i++)
+    {
+        printf("%dn", vet[i]);
+    }
+
+    system("pause");
+    return 0;
 }
- 
-// Função de Ordenação por Inserção
-// Insertion sort function
-void insertion_sort(int *a)
-{
- int i, j, tmp;
- 
- for(i = 1; i < MAX; i++)
- {
-  tmp = a[i];
-  for(j = i-1; j >= 0 && tmp < a[j]; j--)
-  {
-   a[j+1] = a[j];
-  }
-  a[j+1] = tmp;
- }
+
+void insertion_sort(int *a){
+    int i, j, tmp;
+    for(i = 1; i < MAX; i++)
+    {
+        tmp = a[i];
+        for(j = i-1; j >= 0 && tmp < a[j]; j--)
+        {
+            a[j+1] = a[j];
+        }
+    a[j+1] = tmp;
+    }
 }
+   // Funcionamento: A ordenação por inserção é baseada na ideia de que um elemento dos elementos de entrada é consumido em cada iteração para encontrar sua posição correta, ou seja, a posição à qual pertence em uma matriz ordenada.
+   // - Esse algoritmo é bastante simples e possui uma fácil implementação. Ele é eficiente para uma quantidade pequena de dados.
+   // - Para N elementos, há N² comparações. O tempo de complexidade é O(N²).
